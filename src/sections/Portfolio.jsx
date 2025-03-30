5f866d93b5fb543c0812a30197d2f5aea209d022
DreamCaptureEnt <dreamcapture2025@gmail.com>
2025-03-30 11:21:44 +0530

    Update Portfolio.jsx
    dk

--- src/sections/Portfolio.jsx
+++ src/sections/Portfolio.jsx
@@ -1,31 +1,28 @@
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/all'
-import { useGSAP } from '@gsap/react'
-import Works from '../components/Works'
-function Portfolio() {
-    gsap.registerPlugin(ScrollTrigger);
-    useGSAP(() => {
-      gsap.from('.split-title', {
-        scale: 1.2,
-        ease: 'power3.inOut',
-        scrollTrigger: {
-          trigger: '.split-title',
-          start: 'top bottom',
-          end: 'top 40%',
-          scrub: true,
-        }
-      })
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useGSAP } from "@gsap/react";
+import Works from "../components/Works";
 
-    })
+function Portfolio() {
+  gsap.registerPlugin(ScrollTrigger);
+  useGSAP(() => {
+    gsap.from(".split-title", {
+      scale: 1.2,
+      ease: "power3.inOut",
+      scrollTrigger: {
+        trigger: ".split-title",
+        start: "top bottom",
+        end: "top 40%",
+        scrub: true,
+      },
+    });
+  });
 
   return (
-   <section id='portfolio'
-   >
-     <Works />
-
-   </section>
-  )
+    <section id="portfolio" className="min-h-screen w-full bg-black text-white">
+      <Works />
+    </section>
+  );
 }
 
-export default Portfolio
-
+export default Portfolio;
